33308c4bc7fd8028646a0544895facc2377646e7
frassenator <80252503+frassenator@users.noreply.github.com>
2023-06-17 20:36:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,9 @@ function getFirstPlayer() {
 
 function authenticateHost() {
     let password = document.getElementById("password").value;
-    if (password === "password") {
+    if (password == "password") {
         isHost = true;
+        alert("Logged in as host");
         document.getElementById("hostControls").style.display = "none";
         document.getElementById("hostView").style.display = "block";
         document.getElementById("resetButton").disabled = false;
